cdecce4329b910e07abe92c0297349af5db1c1c9
Advait Nair <advaitincorporated@gmail.com>
2025-07-30 16:51:46 +0530

    Bump version to 0.2.8 and update scale value in Card component for improved styling

--- src/lib/organise/Card.svelte
+++ src/lib/organise/Card.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { removeClass } from '$lib/utils/Utils.js';
     // import '../styling/component_styling/organise/main.scss';
-    let { children = ()=>{}, bg='', bgIntensity=.5, aura=false, xl=false, md=false, glass=false, dark=false, z=1, mainContentClass='', wrapperClass='', nofw=false, scale=1.05, ...props } = $props();
+    let { children = ()=>{}, bg='', bgIntensity=.5, aura=false, xl=false, md=false, glass=false, dark=false, z=1, mainContentClass='', wrapperClass='', nofw=false, scale=1.5, ...props } = $props();
 
     let filterControl = $state('');
     if (bgIntensity > 0)
@@ -19,7 +19,7 @@
         <!-- <div class="jo-backdrop-filter-controller" style="{filterControl}"></div> -->
         <div class="jo-bg w-full" style="background-image: url({bg}); {filterControl}"></div>
         {#if aura}
-        <div class="jo-bg-blur w-full" style="background-image: url({bg}); opacity: {bgIntensity*1.2}; transform: scale({scale});"></div>
+        <div class="jo-bg-blur w-full pointer-events-none select-none" style="background-image: url({bg}); opacity: {bgIntensity*1.2}; transform: scale({scale});"></div>
         {/if}
     {/if}
 </div>
